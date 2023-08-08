56c2524dcf79b35b74c13ee8e7eab71a6dde7406
Herman Jangsett Mostein <herman.mostein@hotmail.com>
2023-08-08 13:01:30 +0200

    satt opp

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className={[styles.h1]}>
+            Hei
+        </h1>
+    </div>
+  )
+}
